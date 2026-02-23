67737848e88d8d04e751ade98f823c1d92877e67
Pranav <83500881+pranavgovi@users.noreply.github.com>
2026-02-23 01:14:01 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,39 +1,39 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        left=0
-        right=0
-        star=0
-        #left traversal
+        
+        star =0
+        o,c = 0,0
         for i in s:
             if i=='(':
-                left+=1
-            elif i==')':
-                right+=1
-            else:
+                o+=1
+            elif i=='*':
                 star+=1
-            if right-left>0:
-                #this means that close is greater than open
-                #i have to use a star
-                if not star:
-                    return False
-                star-=1
-                left+=1
-                
-        n=len(s)
-        left,right,star=0,0,0
-        for i in range(n-1,-1,-1):
-            if s[i]==')':
-                right+=1
-            elif s[i]=='(':
-                if right>0:
-                    right-=1
-                elif star>0:
+            else:
+                if o:
+                    o-=1
+                elif star:
                     star-=1
                 else:
+                    #nothing is there
                     return False
-            else:
+        c=0
+        start=0
+        for i in range(len(s)-1,-1,-1):
+            if s[i]==')':
+                c+=1
+            elif s[i]=='*':
                 star+=1
+            else:
+                if c:
+                    c-=1
+                elif star:
+                    star-=1
+                else:
+                    #nothing is there
+                    return False
+     
         return True
-        
+            
 
         
+    
