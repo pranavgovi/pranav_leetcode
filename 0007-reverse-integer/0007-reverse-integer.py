264a8da36a0d70cb60297339a1d5862a402e752a
Pranav <83500881+pranavgovi@users.noreply.github.com>
2026-03-04 00:46:10 -0500

    Time: 52 ms (18.54%), Space: 19.4 MB (7.96%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,25 +1,21 @@
 class Solution:
     def reverse(self, x: int) -> int:
         MAX, MIN = (2**31)-1, -(2**31)
-
         neg=False
         if x<0:
             neg= True
-        x=abs(x)
-        answer =0 
         
+        answer =0 
+    
         while x:
-            curr_digit = x%10
-            x= x//10
-
+            curr_digit = int(math.fmod(x, 10)) #for negative numbers also int is used becuase float can be returned
+            if answer>int(MAX/10) or (answer==int(MAX/10) and curr_digit>7):
+                return 0
+            if answer<int(MIN/10) or answer==int(MIN/10) and curr_digit<-8:
+                return 0
+            #before building the answer check
             #now builidng the answer
             answer= answer*(10) + curr_digit
-        if neg:
-            answer=-answer
-
-        if answer> MAX or answer<MIN:
-            return 0
-        else:
-            return answer
-            
-
+            x=int(x/10) #x//10 will truncate towars -inf
+        return answer
+       
