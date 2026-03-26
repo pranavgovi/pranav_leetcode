17d31f9ea331c1bc79f263b8f4d059a0cde13d41
Pranav <83500881+pranavgovi@users.noreply.github.com>
2026-03-26 11:27:46 -0400

    Time: 110 ms (47.89%), Space: 21.2 MB (70.89%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.py
+++ 2013-detect-squares/2013-detect-squares.py
@@ -1,29 +1,27 @@
 class DetectSquares:
 
     def __init__(self):
-        self.points=defaultdict(int)
-
+        self.lookup = {}
 
     def add(self, point: List[int]) -> None:
-        self.points[tuple(point)]+=1
-    
+        pt=tuple(point)
+        if pt not in self.lookup:
+            self.lookup[pt]=0
+        self.lookup[tuple(point)]+=1
+
+
     def count(self, point: List[int]) -> int:
-        answer=0
-        #for a point check if there is a diagonal point except self
-        x1 , y1= point
-        for p, count in self.points.items():
-            x2, y2 = p
-            if abs(x1-x2) == abs(y1-y2) and x1!=x2:
-                #this is a diagonal point
-                if (x2, y1) in self.points and (x1, y2) in self.points:
-                   answer+=(
-                    count*self.points[(x2, y1)]*self.points[(x1,y2)]
-                   ) 
-
-        return answer
-
-
-        
+        #go over checking every single diagonal pt
+        #cumukate the ways
+        ways=0
+        x1, y1= point
+        for pt, count in self.lookup.items():
+            x2, y2 = pt
+            if abs(x1-x2)==abs(y1-y2) and x1!=x2:
+                #x2,y2 is a diagonal pt to x1,y1
+                if (x1,y2) in self.lookup and (x2,y1) in self.lookup:
+                    ways+= (self.lookup[(x2, y2)] * self.lookup[(x2,y1)]* self.lookup[(x1,y2)])
+        return ways
 
 
 # Your DetectSquares object will be instantiated and called as such:
