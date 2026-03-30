d7eee30dd7522a3fe470e51ba2481f8312562934
Pranav <83500881+pranavgovi@users.noreply.github.com>
2026-03-30 12:24:55 -0400

    Time: 5 ms (79.5%), Space: 23.5 MB (6.28%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,18 +1,19 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        n = len(intervals)
         answer=[]
-        intervals.sort()
-        n=len(intervals)
-        a,b = intervals[0][0], intervals[0][1]
-        for i in range(1,n):
-            c,d = intervals[i][0], intervals[i][1]
-            if b>=c:
-                #overlap
-                a,b =a , max(b,d)
-            else:
-                #just append
-                answer.append([a,b])
-                a,b = c,d
-        answer.append([a,b])
-        return answer
+        intervals.sort(key = lambda interval: (interval[0], interval[1]) )
+        prev_start, prev_end = intervals[0][0], intervals[0][1]
 
+        for i in range(1,n):
+            curr_start, curr_end = intervals[i][0], intervals[i][1]
+            if curr_start>prev_end:
+                #no overlap
+                #we can append the (prev) to the answer
+                answer.append([prev_start, prev_end])
+                prev_start, prev_end= curr_start, curr_end
+            
+            else:
+                prev_end = max(prev_end, curr_end)
+        answer.append([prev_start, prev_end])
+        return answer
