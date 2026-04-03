3cbb6b92d9755e58bff0bfcf5b70ac64b8bb2199
Pranav <83500881+pranavgovi@users.noreply.github.com>
2026-04-02 22:12:50 -0400

    Time: 23 ms (48.33%), Space: 19.3 MB (92.12%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,40 +1,49 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        n=len(s2)
-        n1=len(s1)
-        if len(s1)>len(s2):
-            return False
-        needed=defaultdict(int)
-        having={}
+        #this is a fixed window variant
+
+        needed ={}
+        having ={}
+        have=0
         for i in s1:
-            needed[i]+=1
-    
-        in_hand=0
-        left,right=0,0
-        while left<=right and right<n:
-            if s2[right] not in needed:
-                right+=1
-                left=right
-                having={}
+            if i not in needed:
+                needed[i]=1
             else:
-                if s2[right] not in having:
-                    having[s2[right]]=0
-                
-                having[s2[right]]+=1
-               
-                if having[s2[right]]>needed[s2[right]]:
-                    #move the left pointer till having==needed
-                    while having[s2[right]]>needed[s2[right]]:
-                        having[s2[left]]-=1
-                        left+=1
-                    
-                
-                if right-left+1==n1:
-                    return True
-                right+=1
-                  
-                            
-        return False
+                needed[i]+=1
+        
+        left=0
+        for right in range(len(s2)):
+
+            if s2[right] not in needed:
+                #we dont need this char
+                have=0
+                having ={}
+                left=right+1
             
+            else:
+
+                having[s2[right]]  = having.get(s2[right], 0) + 1
+
+                if having[s2[right]] == needed[s2[right]]:
+                    have+=1
+                
+
+                while right-left+1 > len(s1):
+                    #this exceeds the length
+
+                    #move left
+                    if s2[left] in having:
+                        if having[s2[left]] == needed[s2[left]]:
+                            have-=1
+                        having[s2[left]]-=1
+                    left+=1
+
+                if right-left+1 == len(s1) and have == len(needed):
+                    return True
         
+            
+        return False
         
+
+    
+
