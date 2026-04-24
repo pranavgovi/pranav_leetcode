36bc6cacc733c41532df04c498641abd3d549df4
Pranav <83500881+pranavgovi@users.noreply.github.com>
2026-04-24 15:42:54 -0400

    Time: 347 ms (39.27%), Space: 21.5 MB (28.66%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,30 +1,28 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        #basically we r gonna compute the shortest path for all nodes from start
-        #the node with maximum time will be our total min overall
-
+        #we have to make sure we reach all the nodes in shortest time possible return the maximum of the times
+        min_heap=[]
+        heapq.heappush(min_heap, (0,k)) #we start at k where t=0
         neighbors=defaultdict(list)
-        for time in times:
-            source, target, distance = time
-            neighbors[source].append((distance,target))
-        
-        myheap=[]
-        heapq.heappush(myheap, (0,k))
+        for node in times:
+            src, dest, time =node
+            neighbors[src].append([dest, time]) 
+
         visited=set()
-
-        ans = 0
-        while len(visited)!=n and myheap:
-
-            dist , node = heapq.heappop(myheap)
-            ans=max(ans,dist)
-            if not node in visited:
-                visited.add(node)
-                #while iterating through neighbors of node while pushing bACK ADD THIS DIST
-                arr = neighbors[node]
-                for neighbor in arr:
-                    distance , neighbor_node=neighbor
-                    heapq.heappush(myheap,(distance+dist , neighbor_node))
+        ans=0
+        while min_heap:
+            t , node= heapq.heappop(min_heap)
+            if node in visited:
+                continue
+            ans=max(ans, t)
+            visited.add(node)
+            for nei in neighbors[node]:
+                nd , time_taken =nei
+                if nd not in visited:
+                    heapq.heappush(min_heap,(t+time_taken, nd) )
         
         if len(visited)!=n:
             return -1
         return ans
+        
+            #while pushing neighbors 
