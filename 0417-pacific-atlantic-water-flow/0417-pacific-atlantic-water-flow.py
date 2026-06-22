f4dafeb1e80273302a2cab0267723ac6068ad457
Pranav <83500881+pranavgovi@users.noreply.github.com>
2026-06-22 19:37:27 -0400

    Time: 43 ms (20.77%), Space: 20.7 MB (86.29%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -2,7 +2,7 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         directions = [(1,0), (0,1), (-1,0), (0, -1)]
         m,n = len(heights), len(heights[0])
-        def ocean_flow(visited, results, queue):
+        def ocean_flow(visited,queue):
             while queue:
                 x = len(queue)
                 for i in range(x):
@@ -12,32 +12,30 @@ class Solution:
                         new_r, new_c = a+r, b+c
                         if 0<=new_r<m and 0<=new_c<n and (new_r, new_c) not in visited and heights[new_r][new_c] >= heights[r][c]:
                             queue.append((new_r, new_c))
-                            results[new_r][new_c]=1
+                            #only cells taht are reachbale are added
                             visited.add((new_r, new_c))
                             #this means that from r,c -> new_r, new_c can be rewacjed
             return
         pacific_visited = set()
         atlantic_visited = set()
-        pacific_results = [[0]*n for _ in range(m)]
-        atlantic_results = [[0]*n for _ in range(m)]
         pacific_queue = deque()
         atlantic_queue = deque()
         for i in range(m):
             for j in range(n):
                 if j==0 or i==0:
-                    pacific_results[i][j]=1
+                 
                     pacific_queue.append((i,j))
                     pacific_visited.add((i,j))
                 if i==m-1 or j==n-1:
-                    atlantic_results[i][j]=1
+            
                     atlantic_queue.append((i,j))
                     atlantic_visited.add((i,j))
-        ocean_flow(pacific_visited, pacific_results, pacific_queue)
-        ocean_flow(atlantic_visited, atlantic_results, atlantic_queue)
+        ocean_flow(pacific_visited, pacific_queue)
+        ocean_flow(atlantic_visited, atlantic_queue)
         answer=[]
         for i in range(m):
             for j in range(n):
-                if pacific_results[i][j] and atlantic_results[i][j]:
+                if (i,j) in pacific_visited and (i,j) in atlantic_visited:
                     answer.append([i,j])
         return answer
 
