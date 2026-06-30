11c8948fc6e6e18e9a531f3b7db763beae3f9520
Pranav <83500881+pranavgovi@users.noreply.github.com>
2026-06-29 23:22:56 -0400

    Time: 0 ms (100%), Space: 19.1 MB (98.93%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,19 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        lookup = {'{':'}', '[':']', '(':')'}
+        lookup={
+            '{':'}', '[':']', '(':')'
+        }
         stack=[]
         for i in s:
-            if i not in lookup:
-                #it is closed
+            if i in lookup:
+                #it is a open
+                stack.append(i)
+            else:
+                #it is a closed paranthesis
                 if not stack:
                     return False
-                top =  stack[-1]
-                if top not in lookup or lookup[top]!= i:
+                if stack and lookup[stack[-1]]!=i:
                     return False
                 stack.pop()
-            else:
-                stack.append(i)
         if stack:
             return False
-        else:
-            return True
+        return True
