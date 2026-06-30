9507830a1fe67a040330e449804e4051adb1205e
Pranav <83500881+pranavgovi@users.noreply.github.com>
2026-06-29 20:38:18 -0400

    Time: 15 ms (81.18%), Space: 19.3 MB (85.8%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -4,7 +4,7 @@ class Solution:
 
         needed ={}
         having ={}
-        have=0
+
         for i in s1:
             if i not in needed:
                 needed[i]=1
@@ -16,29 +16,19 @@ class Solution:
 
             if s2[right] not in needed:
                 #we dont need this char
-                have=0
-                having ={}
                 left=right+1
-            
+                having={}
             else:
 
                 having[s2[right]]  = having.get(s2[right], 0) + 1
-
-                if having[s2[right]] == needed[s2[right]]:
-                    have+=1
-                
-
                 while right-left+1 > len(s1):
-                    #this exceeds the length
-
-                    #move left
+                    
                     if s2[left] in having:
-                        if having[s2[left]] == needed[s2[left]]:
-                            have-=1
                         having[s2[left]]-=1
+                        
                     left+=1
 
-                if right-left+1 == len(s1) and have == len(needed):
+                if needed== having:
                     return True
         
             
