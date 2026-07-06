28088b0f447c151b075e1d30db2173d287afc211
Pranav <83500881+pranavgovi@users.noreply.github.com>
2026-07-06 18:50:17 -0400

    Time: 35 ms (38.44%), Space: 20.8 MB (66.78%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,44 +1,34 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        directions = [(1,0), (0,1), (-1,0), (0, -1)]
         m,n = len(heights), len(heights[0])
-        def ocean_flow(visited,queue):
+        pacific_queue=deque()
+        atlantic_queue = deque()
+        pacific_visited=[[0]*n for i in range(m)]
+        atlantic_visited = [ [0]*n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i==0 or j==0:
+                    pacific_visited[i][j]=1
+                    pacific_queue.append((i,j))
+                if i==m-1 or j==n-1:
+                    atlantic_visited[i][j]=1
+                    atlantic_queue.append((i,j))
+        directions =[(1,0), (0,1), (-1,0), (0,-1)]
+        def traverse(visited, queue):
+
             while queue:
-             
                 r,c = queue.popleft()
                 for dir in directions:
-                    a,b =dir
-                    new_r, new_c = a+r, b+c
-                    if 0<=new_r<m and 0<=new_c<n and (new_r, new_c) not in visited and heights[new_r][new_c] >= heights[r][c]:
+                    a,b = dir
+                    new_r, new_c = r+a, c+b
+                    if 0<=new_r<m and 0<=new_c<n and heights[new_r][new_c]>= heights[r][c] and not visited[new_r][new_c]:
                         queue.append((new_r, new_c))
-                        #only cells taht are reachbale are added
-                        visited.add((new_r, new_c))
-                        #this means that from r,c -> new_r, new_c can be rewacjed
-            return
-        pacific_visited = set()
-        atlantic_visited = set()
-        pacific_queue = deque()
-        atlantic_queue = deque()
+                        visited[new_r][new_c]=1
+        traverse(pacific_visited, pacific_queue)
+        traverse(atlantic_visited, atlantic_queue)
+        ans=[]
         for i in range(m):
             for j in range(n):
-                if j==0 or i==0:
-                 
-                    pacific_queue.append((i,j))
-                    pacific_visited.add((i,j))
-                if i==m-1 or j==n-1:
-            
-                    atlantic_queue.append((i,j))
-                    atlantic_visited.add((i,j))
-        ocean_flow(pacific_visited, pacific_queue)
-        ocean_flow(atlantic_visited, atlantic_queue)
-        answer=[]
-        for i in range(m):
-            for j in range(n):
-                if (i,j) in pacific_visited and (i,j) in atlantic_visited:
-                    answer.append([i,j])
-        return answer
-
-                
-
-
-            #this function populates the results array denoting cells that can reach the ocean
+                if pacific_visited[i][j] and atlantic_visited[i][j]:
+                    ans.append((i,j))
+        return ans
