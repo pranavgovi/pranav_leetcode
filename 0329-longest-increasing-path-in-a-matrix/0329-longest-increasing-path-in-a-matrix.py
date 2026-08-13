5b966b5ebefd7089d033da1ee56fcc647dc8f60c
Pranav <83500881+pranavgovi@users.noreply.github.com>
2026-08-12 21:52:47 -0400

    Time: 145 ms (38.85%), Space: 36.7 MB (15.16%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -4,19 +4,19 @@ class Solution:
         memo={}
         directions = [(1,0), (0,1), (-1,0), (0,-1)]
         m,n = len(matrix), len(matrix[0])
-        visited=set()
+    
         def dp(i,j):
             if (i,j) in memo:
                 return memo[(i,j)]
             #for any i,j
             max_len = 1 #by not moving , staying at (i,j)
-            visited.add((i,j))
+        
             for d in directions:
                 a,b = d
                 new_r, new_c= a+i, b+j
-                if 0<=new_r<m and 0<=new_c<n and (new_r, new_c) not in visited and matrix[new_r][new_c]>matrix[i][j]:
+                if 0<=new_r<m and 0<=new_c<n and matrix[new_r][new_c]>matrix[i][j]:
                     max_len = max(max_len, 1+ dp(new_r, new_c))
-            visited.remove((i,j))
+            
             memo[(i,j)]=max_len
             return max_len
         maxi=1
