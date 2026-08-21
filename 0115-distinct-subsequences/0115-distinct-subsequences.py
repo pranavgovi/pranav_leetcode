312c5b0c970eaf69a193b4058feba0dc1600b286
Pranav <83500881+pranavgovi@users.noreply.github.com>
2026-08-21 11:01:49 -0400

    Time: 857 ms (12.09%), Space: 232.9 MB (11.06%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,46 +1,38 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        m,n = len(s), len(t)
+        if len(s)<len(t):
+            return 0
         
-    
-        # def backtrack(ind1, ind2):
-        #     nonlocal ways
-
-        #     if ind2==n:
-        #         #i reached the end
-        #         ways+=1
-        #         return 
-        #     if ind1>=m:
-        #         return
-        #     if s[ind1]==t[ind2]:
-        #         #i can either include or skip
-        #         backtrack(ind1+1, ind2)
-        #         backtrack(ind1+1, ind2+1)
-            
-        #     else:
-        #         backtrack(ind1+1, ind2)
-        # backtrack(0,0)
-        # return ways
-        #lets memoize from ind1, ind2 how many ways are there to reach end
         memo={}
-        def dp(ind1, ind2):
-            if (ind1, ind2) in memo:
-                return memo[(ind1, ind2)]
-            if ind2==n:
-                #it reached the end
-                memo[(ind1, ind2)] =1
+
+        m,n = len(s), len(t)
+
+        def dp(s_ind, t_ind):
+            if (s_ind, t_ind) in memo:
+                return memo[(s_ind, t_ind)]
+            if t_ind==n:
+                memo[(s_ind, t_ind)]=1
                 return 1
-                #1 way possible
-            if ind1>=m:
-                memo[(ind1, ind2)]=0
+            
+            #if s completes before t
+            if s_ind==m:
+                memo[(s_ind, t_ind)]=0
                 return 0
             
-            if s[ind1]==t[ind2]:
-                memo[(ind1, ind2)] = dp(ind1+1, ind2+1) +dp(ind1+1, ind2)
-                return memo[(ind1, ind2)]
+            #check if both of them match
+            tot=0
+            if s[s_ind]==t[t_ind]:
+                #we have 2 chocies to make
+                #either take it
+                tot = dp(s_ind+1, t_ind+1)
+                #skip it
+                tot +=dp(s_ind+1, t_ind)
             
             else:
-                memo[(ind1, ind2)] = dp(ind1+1, ind2)
-                return memo[(ind1, ind2)]
+                tot = dp(s_ind+1, t_ind)
+            memo[(s_ind, t_ind)]=tot
+            return tot
+        
         return dp(0,0)
 
+            
