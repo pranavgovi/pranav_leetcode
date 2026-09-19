5efda1c5b478f9f1194f224edb362d0ddcb23da5
Pranav <83500881+pranavgovi@users.noreply.github.com>
2026-09-19 14:06:30 -0400

    Time: 0 ms (100%), Space: 19.4 MB (7.93%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,41 +1,30 @@
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        
-        #top can range from 0 -> len(matrix)-1
-        #bottm len(matrix)-1 towrads top
-        #top should always be < bottom
-        #left ranges from 0 -> n(columns)-1
-        #right -> vice versa
-        #left should alwyas < right
-        answer=[]
-        m,n = len(matrix), len(matrix[0])
-        top, left = 0, 0
-        bottom, right = m-1, n-1
-        while top<=bottom and left<=right:
-            #process left to right and inc top
-            for i in range(left, right+1):
-                answer.append(matrix[top][i])
-            top+=1
-            #now process top to bottom and inc right
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+        ans =[]
+        m,n= len(matrix), len(matrix[0])
+        top=0
+        left=0
+        right =n-1
+        bottom=m-1
 
-            for i in range(top, bottom+1):
-                answer.append(matrix[i][right])
+        while top<=bottom and left<=right:
+            #good condition to start with
+            for col in range(left, right+1):
+                ans.append(matrix[top][col])
+            top+=1
+
+            #you go from top to bottom and you finissh a a right column
+            for row in range(top, bottom+1):
+                ans.append(matrix[row][right])
             right-=1
 
-            #now process right to left and decrease bottom
-            #to not process same top, bootm
-            if top<=bottom:
-                for i in range(right, left-1, -1):
-                    answer.append(matrix[bottom][i])
+            if top<=bottom and left<=right:
+                for col in range(right, left-1,-1):
+                    ans.append(matrix[bottom][col])
                 
                 bottom-=1
-            #proces bottom to top and move left
-            if left<=right:
-                for i in range(bottom, top-1, -1):
-                    answer.append(matrix[i][left])
+            
+                for row in range(bottom, top-1,-1):
+                    ans.append(matrix[row][left])
                 left+=1
-        return answer
-
-
-
-
+        return ans
